4413a0e7eea92f7a995cf6231d49b605232a6c45
unknown <mecm06.mm@gmail.com>
2021-07-09 16:28:41 +0800

    Added REST Api

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/success', function () {
+    return view('layout');
+});
+
+Route::get('/basketball', function () {
+    return view('basketball');
+});
+
+Route::get('/anime', function () {
+    return view('anime');
+});
+
+Route::get('/tvseries', function () {
+    return view('tvseries');
+});
